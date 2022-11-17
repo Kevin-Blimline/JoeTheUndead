02cfe6eaf0a6f4aa1850601f2884e6400c94fd18
Kevin Blimline <kblimy2@gmail.com>
2022-11-17 18:11:12 -0500

    First push, basic functioning

---
+++ main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 16 20:57:37 2022
+
+@author: Kevin
+"""
+
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '!')
+
+@client.event
+async def on_ready():
+    #await client.change_presence(status = discord.Status.online, activity = discord.Game('Plotting his revenge'))
+    print('Bot is online')
+
+@client.command(aliases = ['wheresmatt', 'Wheresmatt', 'WhereIsMatt', 'whereismatt'], brief = 'Tells user where Matt Crump is')
+async def WheresMatt(ctx):
+    myid = '<@160482712807931904>'
+    await ctx.send('*BANG*')
+    await ctx.send(myid + " is on the couch")
+
+@client.command(aliases = ['shootmason', 'Shootmason', 'shootMason'], brief = 'Shoots Mason')
+async def ShootMason(ctx):
+    myid = '<@295250323893780481>'
+    await ctx.send('*BANG*')
+    await ctx.send(myid + " has died")
+    
+client.run('MTA0MjYxMjA0OTIwNjgzNzI5OQ.GUPhcF.FVRatlDLgP9XfmhfSy7K-_2B7pKPx2aHa4pN9Q')
